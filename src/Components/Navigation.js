72a0ea3127610e43268e765f9c4946180596b970
rashliving <rahulshukla1184@gmail.com>
2022-09-14 00:33:45 +1000

    Changed logo. About us page content updated.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -11,9 +11,10 @@ const Navigation = ({active}) => {
         <Container>
         <Navbar.Brand href="/">
           <Image src={Logo} width="100%" className="app-navbar-image"></Image>
+          <NavLink to="/" className={({isActive}) => isActive ? "nav-link active nav-link-home" :  "nav-link  nav-link-home" }>Netspruze</NavLink>
         </Navbar.Brand>
         <Nav className="me-right">
-          <NavLink to="/" className={({isActive}) => isActive ? "nav-link active" :  "nav-link" }>Home</NavLink>
+          {/* <NavLink to="/" className={({isActive}) => isActive ? "nav-link active" :  "nav-link" }>Home</NavLink> */}
           <NavLink to="/products" className={({isActive}) => isActive ? "nav-link active" :  "nav-link" }>Products</NavLink>
           <NavLink to="/contact-us" className={({isActive}) => isActive ? "nav-link active" :  "nav-link" }>Contact Us</NavLink>
           <NavLink to="/about-us" className={({isActive}) => isActive ? "nav-link active" :  "nav-link" }>About Us</NavLink>
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -6,18 +6,21 @@ const AboutUs = () => {
         <>
         <Container className="main-content">
         <div className="about-us">
-            <h1> About Us</h1>
-            <p>At Netspruze, At Netspruze, there’s nothing we enjoy more than helping customers solve business issues through effective use of technology. 
-                We specialize in helping small, midsize and large companies gain a competitive advantage by being more efficient, gaining a crystal-clear 
-                view of their business, and mitigating technology risks. Let us handle all of the complexities so you can focus on your core business.
-            </p>
-            <p>Our team’s diverse expertise and experience means that you are quickly up and running with solutions that meet your needs.</p>
-            <p>Netspruze Limited operates in four core business areas.</p>
+            <h1>Netspruze LLC.</h1>
+            <hr></hr>
+            <h6 class="about-us-subheading">1922 Pheasant Dr Elko, Nevada USA. 775-299-1767. 650-680-5367</h6>
+            <p>At Netspruze LLC. our goal is to help our customers resolve business challenges through effective use of technology. We specialize in helping organizations gain competitive advantage to become more efficient, and gain control of their business while mitigating technology risks.</p>
+            <p>With over 10 years’ experience in supply chain, engineering designs and heavy equipment supplies, our multinational business with branches in the United States of America and Australia has partnered with world class heavy equipment producers and suppliers such as Cashman, Joy Global, Caterpillar, Komatsu, Hitachi, John Deere to mention a few.</p>
+            <p>Our Heavy Equipment Supply include:</p>
             <ul>
-                <li>Information Technology services</li>
-                <li>Security systems</li>
-                <li>QHSE Training and PPE supplies</li>
-                <li>Instrumentation systems (installation & services)</li>
+                <li>Cranes</li>
+                <li>Excavators</li>
+                <li>Drilling Equipment</li>
+                <li>Manlifts</li>
+                <li>Sidebooms</li>
+                <li>Dozers</li>
+                <li>Haul trucks</li>
+                <li>Power Systems (Generators &amp; Turbines)</li>
             </ul>
         </div>
         </Container>
